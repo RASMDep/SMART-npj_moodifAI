235b042dc32903a6b340eb989f08c2440df45374
gdapoian <giulia.dap@gmail.com>
2023-11-15 15:22:08 +0100

    remove attention

--- models/model_Conv1dNet.py
+++ models/model_Conv1dNet.py
@@ -21,10 +21,7 @@ class Conv1dNet(torch.nn.Module):
 
         #self.criterion = nn.BCEWithLogitsLoss()#pos_weight=torch.tensor([0.7]))
         self.criterion = nn.CrossEntropyLoss()
-
-        # Define the attention mechanism
-        self.attention_layer = nn.MultiheadAttention(embed_dim=256, num_heads=4)
-
+        
         # 12 is the input space in terms of ECG leads, 16 is the output space corresponding to the new features
         # Conv1d(input size==ecg channels, outputs size , filter size)
         self.block1 = nn.Sequential(nn.Conv1d(self.in_size, 16, 5, stride=1, padding = 2),nn.ReLU(),nn.MaxPool1d(2, stride=2),nn.BatchNorm1d(16))
@@ -55,12 +52,6 @@ class Conv1dNet(torch.nn.Module):
         z = self.block4(z)
         z = self.block5(z)
 
-        # Apply attention mechanism
-        #z = z.permute(2, 0, 1)  # Change the order of dimensions
-        #z, _ = self.attention_layer(z, z, z)  # Self-attention
-        #z = z.permute(1, 2, 0)  # Change the order of dimensions back
-
-        #z = z*mask_d + (1-mask_d)*(-1e6)
         z = self.lastpool(z).squeeze(2)
         if self.n_feat>0:
             # add additional hand crafted features before last layer, do not forget to normalize these features (0 mean, 1 variance)
